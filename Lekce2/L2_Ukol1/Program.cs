a8e4d3bca0abcac3db5c49757ef96af1c4e5cb68
jirrickcz <149876267+jirrickcz@users.noreply.github.com>
2023-11-04 13:07:08 +0100

    L2_Ukol1

---
+++ Lekce2/L2_Ukol1/Program.cs
@@ -0,0 +1,7 @@
+﻿// 1. Vytvoř tři proměnné, pro každý typ jednu(int, double a string). Následně do nich přiřaď hodnotu.Nakonec obsah proměnných vypiš na obrazovku.
+int cislo = 1;
+double cislo2 = 2.0;
+string text = "test";
+Console.WriteLine(cislo);
+Console.WriteLine(cislo2);
+Console.WriteLine(text);
