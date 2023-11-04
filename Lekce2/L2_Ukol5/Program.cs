e47d84b018032fcf0aa445ae005f4a37cdce7d0d
jirrickcz <149876267+jirrickcz@users.noreply.github.com>
2023-11-04 13:20:41 +0100

    L2_Ukol5

---
+++ Lekce2/L2_Ukol5/Program.cs
@@ -0,0 +1,11 @@
+﻿// 5. (Bonus 1) Od uzivatele nacti nacti dve celociselne promenne. Nakonec obě hodnoty sečti a vypiš uživateli na obrazovku.
+Console.WriteLine("Zadej prvni cislo:");
+string input1 = Console.ReadLine();
+int cislo1 = int.Parse(input1);
+
+Console.WriteLine("Zadej druhe cislo:");
+string input2 = Console.ReadLine();
+int cislo2 = int.Parse(input2);
+
+Console.WriteLine("Vyskedek:");
+Console.WriteLine(cislo1 + cislo2);
