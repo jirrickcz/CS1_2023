feaf7988dbdddb9687a1458d609e18b2961c6a26
jirrickcz <149876267+jirrickcz@users.noreply.github.com>
2023-11-04 13:14:56 +0100

    L2_Ukol3

---
+++ Lekce2/L2_Ukol3/Program.cs
@@ -0,0 +1,5 @@
+﻿// 3. Vytvoř proměnnou obsahující text tvého křestního jména (tak aby nezačínal a nekončil mezerou). Vytvoř druhou proměnnou obsahující text tvého příjmení(opět nesmí začínat ani končit mezerou). Nakonec vytvoř proměnnou, ve které bude spojení těchto dvou proměnných s tím, že je spojíš mezerou.Výsledný text vypiš na obrazovku.
+string jmeno = "Jirka";
+string prijemi = "Hudec";
+string celeJmeno = jmeno + " " + prijemi;
+Console.WriteLine(celeJmeno);
