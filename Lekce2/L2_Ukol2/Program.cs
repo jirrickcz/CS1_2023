b017f4aac0f1fcfe7db640f371ef8b7497a1cac9
jirrickcz <149876267+jirrickcz@users.noreply.github.com>
2023-11-04 13:07:58 +0100

    L2_Ukol2

---
+++ Lekce2/L2_Ukol2/Program.cs
@@ -0,0 +1,5 @@
+﻿// 2. Vytvoř dvě celočíselné proměnné a pak jednu další, ve které bude součet hodnot z předchozích dvou proměnných.Výsledek vypiš na obrazovku.
+int cislo1 = 10;
+int cislo2 = 20;
+int cislo3 = cislo1 + cislo2;
+Console.WriteLine(cislo3);
