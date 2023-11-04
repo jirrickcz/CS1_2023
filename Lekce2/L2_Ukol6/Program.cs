56038a1bdee0f627d55d44cc3b282d77a3db33fd
jirrickcz <149876267+jirrickcz@users.noreply.github.com>
2023-11-04 13:22:45 +0100

    L2_Ukol6

---
+++ Lekce2/L2_Ukol6/Program.cs
@@ -0,0 +1,10 @@
+﻿// 6. (Bonus 2) Vytvoř dvě int proměnné, potom do každé z nich ulož nějakou hodnotu.Zkus hodnoty v těch dvou proměnných prohodit, aniž bys o některou z nich přišla.Můžeš použít pomocnou proměnnou.
+int cislo1 = 15;
+int cislo2 = 35;
+Console.WriteLine("cislo 1: " + cislo1);
+Console.WriteLine("cislo 2: " + cislo2);
+int pomoc = cislo1;
+cislo1 = cislo2;
+cislo2 = pomoc;
+Console.WriteLine("cislo 1: " + cislo1);
+Console.WriteLine("cislo 2: " + cislo2);
