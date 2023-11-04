ea1b7e2d7f72f468e20e4ab428ba1c9709a33ed5
jirrickcz <149876267+jirrickcz@users.noreply.github.com>
2023-11-04 17:29:27 +0100

    L3_Ukol1

---
+++ Lekce3/L3_Ukol1/Program.cs
@@ -0,0 +1,17 @@
+﻿//Vytvorte cyklus, ktery se uzivatele bude ptat na jeho rok narozeni tak dlouho, dokud uzivatel nezada spravne cislo
+//kdyz zada spravne cislo, vypocitejte jeho vek
+
+int rokNarozeni;
+
+Console.WriteLine("Zadej rok narozeni:");
+string vstup1 = Console.ReadLine();
+bool povedloSe = int.TryParse(vstup1, out rokNarozeni);
+while (povedloSe == false)  //while (!povedloSe)
+{
+    Console.WriteLine("To neni cislo. Zkus to znovu.");
+    string vstup2 = Console.ReadLine();
+    povedloSe = int.TryParse(vstup2, out rokNarozeni);
+}
+
+int vek = 2023 - rokNarozeni;
+Console.WriteLine($"Tvuj vek je: {vek}");
