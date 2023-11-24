6106d26a33205cc6ed6cf1cab7bcfe0c93d067cb
jirrickcz <149876267+jirrickcz@users.noreply.github.com>
2023-11-24 21:43:37 +0100

    L9_Kalkulacka

---
+++ Lekce9/L9_Kalkulacka/Kalkulacka.cs
@@ -0,0 +1,40 @@
+﻿namespace L9_Kalkulacka;
+
+public class Kalkulacka
+{
+    public Kalkulacka() : this(0)
+    {
+    }
+
+    public Kalkulacka(double prvniCislo)
+    {
+        _posledniVysledek = prvniCislo;
+    }
+
+    private double _posledniVysledek;
+
+    public void Secti(double cislo){
+        _posledniVysledek += cislo;
+    }
+
+    public void Odecti(double cislo){
+        _posledniVysledek -= cislo;
+    }
+
+    public void Vynasob(double cislo){
+        _posledniVysledek *= cislo;
+    }
+
+    public void Vydel(double cislo){
+        _posledniVysledek /= cislo;
+    }
+
+    public void Umocni(double cislo){
+        _posledniVysledek = Math.Pow(_posledniVysledek, cislo);
+    }
+
+    public double ZiskejVysledek(){
+        return _posledniVysledek;
+    }
+
+}
---
+++ Lekce9/L9_Kalkulacka/Program.cs
@@ -0,0 +1,62 @@
+﻿using L9_Kalkulacka;
+
+public class Program
+{
+	public static void Main()
+	{
+        double prvniCislo = NactiCislo("prvni");
+        Kalkulacka kalkulacka = new Kalkulacka(prvniCislo);
+
+		while (true)
+		{
+			Console.WriteLine("Zadejte operator (+, -, *, /, ^): Pro ukonceni programu zadejte X.");
+			string operatorZnak = Console.ReadLine();
+            if (operatorZnak.ToUpper() == "X")
+			{
+				Console.WriteLine("Konec programu.");
+				return;
+			}
+			while (!(operatorZnak == "+" || operatorZnak == "-" || operatorZnak == "*" || operatorZnak == "/" || operatorZnak == "^"))
+			{
+				Console.WriteLine("Neplatny operator. Zadejte znovu.");
+				operatorZnak = Console.ReadLine();
+			}
+
+			double dalsiCislo = NactiCislo("dalsi");
+
+            switch (operatorZnak)
+            {
+            case "+":
+                kalkulacka.Secti(dalsiCislo);
+                break;
+            case "-":
+                kalkulacka.Odecti(dalsiCislo);
+                break;
+            case "/":
+                kalkulacka.Vydel(dalsiCislo);
+                break;
+            case "*":
+                kalkulacka.Vynasob(dalsiCislo);
+                break;
+            case "^":
+                kalkulacka.Umocni(dalsiCislo);
+                break;
+            }
+
+            double vysledek = kalkulacka.ZiskejVysledek();
+			Console.WriteLine($"Výsledek: {prvniCislo} {operatorZnak} {dalsiCislo} = {vysledek}");
+            prvniCislo = vysledek;
+		}
+	}
+
+    private static double NactiCislo(string jakeCislo){
+        Console.WriteLine($"Zadejte {jakeCislo} cislo:");
+        bool jeCislo = double.TryParse(Console.ReadLine(), out double cislo);
+        while (!jeCislo)
+        {
+            Console.WriteLine($"Neplatne {jakeCislo} cislo. Zkuste znovu:");
+            jeCislo = double.TryParse(Console.ReadLine(), out cislo);
+        }
+        return cislo;
+    }
+}
